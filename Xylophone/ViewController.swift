8f55e11ba316addf220bedc1b77fac3530be50b4
Shirish Koirala <shirishkoirala@gmail.com>
2022-08-29 12:28:32 +1000

    Boss Challenge Complete: Opacity of button change while pressed to 0.5 and changed back to 1.0 after  0.2 seconds.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(name: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2){
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(name: String) {
